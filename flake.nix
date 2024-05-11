290ca1ca706fca480f8f59ef6ad7ef5304b267bc
jwc20 <choij2015@gmail.com>
2024-05-11 16:17:17 +0900

    Add flake

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Developer environment shell rails app";
+
+  inputs = {
+    nixpkgs = {
+      owner = "NixOS";
+      repo = "nixpkgs";
+      rev = "e6d5772f3515b8518d50122471381feae7cbae36";
+      type = "github";
+    };
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      forAllSupportedSystems =
+        f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+        "arm64-darwin" # mac m1
+      ];
+    in
+
+    {
+      devShells = forAllSupportedSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            shellHook = ''
+              export GEM_HOME=$(pwd)/.gems
+              export PATH=$GEM_HOME/bin:$PATH
+            '';
+            packages = [
+              pkgs.docker
+              pkgs.git
+              pkgs.gnumake
+              pkgs.terraform
+              pkgs.nixpkgs-fmt
+              pkgs.nodejs
+              pkgs.nodePackages.pnpm
+              pkgs.postgresql_11
+              pkgs.ruby_3_1
+              pkgs.imagemagick
+            ];
+          };
+        }
+      );
+    };
+}
